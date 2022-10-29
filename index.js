cf2313591ea006ac15e1c5b5a2f25e440651f1d1
Silver <silverserate64@gmail.com>
2022-10-29 10:45:09 +0800

    [UPDATE] Updated matching so it injects not just in homepage.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Offerup Phones.Trade Bot
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  try to take over the world!
 // @author       You
-// @match        https://offerup.com/
+// @match        https://offerup.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=offerup.com
 // @grant        none
 // @run-at document-end
